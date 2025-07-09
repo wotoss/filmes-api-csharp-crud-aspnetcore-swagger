665ad8ee46fbc8c364ea1215d1e0c2c86799c9fb
WOTO <wotoss10@gmail.com>
2025-07-09 17:26:32 -0300

    documentando apis com swagger e comentarios xml

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -20,8 +20,15 @@ public class FilmeController : ControllerBase
         _mapper = mapper;
     }
 
-
+    /// <summary>
+    /// Adiciona um filme ao banco de dados
+    /// </summary>
+    /// <param name="filmeDto">Objeto com os campos necessários para criação
+    /// de um filme</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="201">Caso inserção seja feita com sucesso</response>
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
     {
         Filme filme = _mapper.Map<Filme>(filmeDto);
